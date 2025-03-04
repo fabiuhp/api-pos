b03f8010b14912b59defd28caa9c8b9d3f506358
fabio pereira <fabiuhp@msn.com>
2025-03-04 10:46:37 -0300

    feat: add ProductInterface and implement product database operations

--- internal/infra/database/interfaces.go
+++ internal/infra/database/interfaces.go
@@ -2,7 +2,17 @@ package database
 
 import "github.com/fabiuhp/api-pos/internal/entity"
 
-type UserInterface interface {
-	Create(user *entity.User) error
-	FindByEmail(email string) (*entity.User, error)
-}
+type (
+	UserInterface interface {
+		Create(user *entity.User) error
+		FindByEmail(email string) (*entity.User, error)
+	}
+
+	ProductInterface interface {
+		Create(product *entity.Product) error
+		FindAll(page, limit int, sort string) ([]entity.Product, error)
+		FindByID(id string) (*entity.Product, error)
+		Update(product *entity.Product) error
+		Delete(id string) error
+	}
+)
---
+++ internal/infra/database/product_db.go
@@ -0,0 +1,55 @@
+package database
+
+import (
+	"github.com/fabiuhp/api-pos/internal/entity"
+	"gorm.io/gorm"
+)
+
+type Product struct {
+	DB *gorm.DB
+}
+
+func NewProduct(db *gorm.DB) *Product {
+	return &Product{
+		DB: db,
+	}
+}
+
+func (p *Product) Create(product *entity.Product) error {
+	return p.DB.Create(product).Error
+}
+
+func (p *Product) FindByID(id string) (*entity.Product, error) {
+	var product entity.Product
+	if err := p.DB.Where("id = ?", id).First(&product).Error; err != nil {
+		return nil, err
+	}
+	return &product, nil
+}
+
+func (p *Product) Update(product *entity.Product) error {
+	_, err := p.FindByID(product.ID.String())
+	if err != nil {
+		return err
+	}
+
+	return p.DB.Save(product).Error
+}
+
+func (p *Product) Delete(id string) error {
+	product, err := p.FindByID(id)
+	if err != nil {
+		return err
+	}
+
+	return p.DB.Delete(product).Error
+}
+
+func (p *Product) FindAll(page, limit int, sort string) ([]entity.Product, error) {
+	var products []entity.Product
+	offset := (page - 1) * limit
+	if err := p.DB.Offset(offset).Limit(limit).Order("created_at" + sort).Find(&products).Error; err != nil {
+		return nil, err
+	}
+	return products, nil
+}
