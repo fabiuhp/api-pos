9ddd4668b1b2099a89a2360d112cfeff2a476fca
fabio pereira <fabiuhp@msn.com>
2025-03-04 09:55:09 -0300

    feat: add user database interface and implementation with unit tests

---
+++ internal/infra/database/user_db_test.go
@@ -0,0 +1,31 @@
+package database
+
+import (
+	"testing"
+
+	"github.com/fabiuhp/api-pos/internal/entity"
+	"github.com/stretchr/testify/assert"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func TestCreateUser(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	if err != nil {
+		t.Error(err)
+	}
+	db.AutoMigrate(&entity.User{})
+	user, _ := entity.NewUser("fabio", "fabio@email.com", "senha")
+	userDB := NewUser(db)
+
+	err = userDB.Create(user)
+	assert.Nil(t, err)
+
+	var userFound entity.User
+	db.First(&userFound, user.ID)
+	assert.Equal(t, user.ID, userFound.ID)
+	assert.Equal(t, user.Name, userFound.Name)
+	assert.Equal(t, user.Email, userFound.Email)
+	assert.Equal(t, user.Password, userFound.Password)
+	assert.NotEqual(t, user.Password, userFound.Password)
+}
