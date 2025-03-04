3a365cdf799355d93e685c72d2fdd5f504df277d
fabio pereira <fabiuhp@msn.com>
2025-03-04 10:02:56 -0300

    feat: add unit test for FindUserByEmail function in user database

--- internal/infra/database/user_db_test.go
+++ internal/infra/database/user_db_test.go
@@ -29,3 +29,24 @@ func TestCreateUser(t *testing.T) {
 	assert.Equal(t, user.Password, userFound.Password)
 	assert.NotEqual(t, user.Password, userFound.Password)
 }
+
+func TestFindUserByEmail(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	if err != nil {
+		t.Error(err)
+	}
+	db.AutoMigrate(&entity.User{})
+	user, _ := entity.NewUser("fabio", "fabio@email.com", "senha")
+	userDB := NewUser(db)
+
+	err = userDB.Create(user)
+	assert.Nil(t, err)
+
+	userFound, err := userDB.FindByEmail(user.Email)
+	assert.Nil(t, err)
+	assert.Equal(t, user.ID, userFound.ID)
+	assert.Equal(t, user.Name, userFound.Name)
+	assert.Equal(t, user.Email, userFound.Email)
+	assert.Equal(t, user.Password, userFound.Password)
+	assert.NotEqual(t, user.Password, userFound.Password)
+}
