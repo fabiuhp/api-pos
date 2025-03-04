1917c4e77f828864597965c19e40199b7a78903e
fabio pereira <fabiuhp@msn.com>
2025-03-03 23:21:56 -0300

    feat: enhance configuration loading and display database driver

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,10 +1,16 @@
 package main
 
-import "github.com/fabiuhp/api-pos/configs"
+import (
+	"fmt"
+
+	"github.com/fabiuhp/api-pos/configs"
+)
 
 func main() {
 	config, err := configs.LoadConfig(".")
 	if err != nil {
 		panic(err)
 	}
+
+	fmt.Println(config.DBDriver)
 }
