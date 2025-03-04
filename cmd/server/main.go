591c2e0e9efbd294daf78b04758578b2e8da00ef
fabio pereira <fabiuhp@msn.com>
2025-03-04 15:48:27 -0300

    feat: implement product creation handler and DTO for product input

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,16 +1,61 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"net/http"
 
 	"github.com/fabiuhp/api-pos/configs"
+	"github.com/fabiuhp/api-pos/internal/dto"
+	"github.com/fabiuhp/api-pos/internal/entity"
+	"github.com/fabiuhp/api-pos/internal/infra/database"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 func main() {
-	config, err := configs.LoadConfig(".")
+	_, err := configs.LoadConfig(".")
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(config.DBDriver)
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	db.AutoMigrate(&entity.Product{}, &entity.User{})
+
+	http.ListenAndServe(":8080", nil)
+}
+
+type ProductHandler struct {
+	ProductDB database.ProductInterface
+}
+
+func NewProductHandler(db *gorm.DB) *ProductHandler {
+	return &ProductHandler{
+		ProductDB: database.NewProduct(db),
+	}
+}
+
+func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
+	var product dto.CreateProductInput
+	err := json.NewDecoder(r.Body).Decode(&product)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	newProduct, err := entity.NewProduct(product.Name, product.Price)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	err = h.ProductDB.Create(newProduct)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
 }
